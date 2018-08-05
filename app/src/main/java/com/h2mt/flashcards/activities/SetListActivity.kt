a242af8178e55905d9162a07fe5d9d4de0bbacc3
Hossameldeen <hossamabdalh@gmail.com>
2018-08-05 15:26:10 +0200

    adding set with parameters

--- app/src/main/java/com/h2mt/flashcards/activities/SetListActivity.kt
+++ app/src/main/java/com/h2mt/flashcards/activities/SetListActivity.kt
@@ -1,5 +1,6 @@
 package com.h2mt.flashcards.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
@@ -21,6 +22,10 @@ import kotlinx.android.synthetic.main.activity_set_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.view.MotionEvent
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.*
 
 
 class SetListActivity: AppCompatActivity(), SetListOperations {
@@ -35,20 +40,9 @@ class SetListActivity: AppCompatActivity(), SetListOperations {
         loadSets()
 
         addSet.setOnClickListener(View.OnClickListener {
-            val setCreateRequest = SetCreateRequest(name="hossam", desc = "hossam desc")
-            SetService.addSet(request = setCreateRequest).enqueue(object: Callback<Void>{
-                override fun onFailure(call: Call<Void>?, t: Throwable?) {
-                    Log.e("ADD_SET", t.toString())
-                }
-
-                override fun onResponse(call: Call<Void>?, response: Response<Void>?) {
-                    Log.i("ADD_SET", "add set repsonse received successfully")
-                    if(response!!.isSuccessful){
-                        Log.i("ADD_SET", "add set repsonse received successfully")
-                    }
-                }
-            })
+            onButtonShowPopupWindowClick(it)
         })
+
     }
 
     override fun openSet(setId: Integer) {
@@ -81,6 +75,49 @@ class SetListActivity: AppCompatActivity(), SetListOperations {
                     }
                 }
             }
+        })
+    }
+
+    fun onButtonShowPopupWindowClick(view: View) {
+
+        // inflate the layout of the popup window
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = inflater.inflate(R.layout.set_add_popup, null)
+
+        // create the popup window
+        val width = LinearLayout.LayoutParams.WRAP_CONTENT
+        val height = LinearLayout.LayoutParams.WRAP_CONTENT
+        val focusable = true // lets taps outside the popup also dismiss it
+        val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(View.OnTouchListener { v, event ->
+            popupWindow.dismiss()
+            true
+        })
+
+
+        var button =  findViewById(R.id.addSett) as Button
+        button.setOnClickListener({
+            var setNameTextView = findViewById(R.id.setName) as TextView
+            var setDescTextView = findViewById(R.id.setDesc) as TextView
+            val setCreateRequest = SetCreateRequest(name=setNameTextView.text.toString(), desc = setDescTextView.text.toString())
+            SetService.addSet(request = setCreateRequest).enqueue(object: Callback<Void>{
+                override fun onFailure(call: Call<Void>?, t: Throwable?) {
+                    Log.e("ADD_SET", t.toString())
+                }
+
+                override fun onResponse(call: Call<Void>?, response: Response<Void>?) {
+                    Log.i("ADD_SET", "add set repsonse received successfully")
+                    if(response!!.isSuccessful){
+                        Log.i("ADD_SET", "add set repsonse received successfully")
+                    }
+                }
+            })
         })
     }
 }
